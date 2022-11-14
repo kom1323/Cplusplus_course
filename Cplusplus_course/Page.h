4a33706809997751ebc8e2606cdddade73315c1f
kom1323 <omer2313@gmail.com>
2022-11-14 19:07:36 +0200

    Added page and status classes

---
+++ Cplusplus_course/Page.h
@@ -0,0 +1,25 @@
+#ifndef  _PAGE
+#define _PAGE
+
+#include "Member.h"
+#include "Status.h"
+
+
+
+class Page
+{
+private:
+	char* name;
+	Member** members_list;
+
+public:
+
+	void func();
+
+
+};
+
+
+
+
+#endif // ! _PAGE
